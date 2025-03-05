6acecae9cd7360388ad78976ce44e4b6ff870d25
FelipeNojony <felipe.deoliveira2011@hotmail.com>
2025-03-04 22:03:56 -0300

    Atualização do projeto

--- src/assets/Componentes/Projetos/Projetos.jsx
+++ src/assets/Componentes/Projetos/Projetos.jsx
@@ -1,102 +1,89 @@
-import './index.css'
+import "./index.css";
 
 export default function Projetos() {
   return (
     <>
-        <h2 className="titulo">Meus Projetos</h2>
+      <h2 className="titulo">Meus Projetos</h2>
 
-<div className="container-projetos">
-    <div className="projeto ativo">
-        <a href="#" target="_blank">
-            <img src="./src/assets/img/515x700.png" alt="Capa do projeto" />
-            <h3>Projeto 1</h3>
+      <div className="container-projetos">
+        <div className="projeto-ativo">
+          <a href="#" target="_blank">
+            <div className="imagem">
+              <img src="./src/assets/img/515x700.png" alt="Capa do projeto" />
+            </div>
+
             <div className="informacoes-projeto">
+              <div className="titulo-projeto">
+                <h2>Wine Experience</h2>
+              </div>
+              <div className="descricao">
                 <p>Projeto feito usando HTML, CSS e JavaScript</p>
                 <p>🔗 Ver no GitHub Pages</p>
+              </div>
             </div>
-        </a>
-    </div>
+          </a>
+        </div>
+      </div>
 
-    <div className="projeto ativo">
-        <a href="#" target="_blank">
-            <img src="./src/assets/img/515x700.png" alt="Capa do projeto" />
-            <h3>Projeto 2</h3>
+      <div className="container-projetos">
+        <div className="projeto-ativo">
+          <a href="#" target="_blank">
+            <div className="imagem">
+              <img src="./src/assets/img/515x700.png" alt="Capa do projeto" />
+            </div>
+
             <div className="informacoes-projeto">
+              <div className="titulo-projeto">
+                <h2>Wine Experience</h2>
+              </div>
+              <div className="descricao">
                 <p>Projeto feito usando HTML, CSS e JavaScript</p>
                 <p>🔗 Ver no GitHub Pages</p>
+              </div>
             </div>
-        </a>
-    </div>
+          </a>
+        </div>
+      </div>
 
-    <div className="projeto ativo">
-        <a href="#" target="_blank">
-            <img src="./src/assets/img/515x700.png" alt="Capa do projeto" />
-            <h3>Projeto 3</h3>
+      <div className="container-projetos">
+        <div className="projeto-ativo">
+          <a href="#" target="_blank">
+            <div className="imagem">
+              <img src="./src/assets/img/515x700.png" alt="Capa do projeto" />
+            </div>
+
             <div className="informacoes-projeto">
+              <div className="titulo-projeto">
+                <h2>Wine Experience</h2>
+              </div>
+              <div className="descricao">
                 <p>Projeto feito usando HTML, CSS e JavaScript</p>
                 <p>🔗 Ver no GitHub Pages</p>
+              </div>
             </div>
-        </a>
-    </div>
+          </a>
+        </div>
+      </div>
 
-    <div className="projeto ativo">
-        <a href="#" target="_blank">
-            <img src="./src/assets/img/515x700.png" alt="Capa do projeto" />
-            <h3>Projeto 4</h3>
+      <div className="container-projetos">
+        <div className="projeto-ativo">
+          <a href="#" target="_blank">
+            <div className="imagem">
+              <img src="./src/assets/img/515x700.png" alt="Capa do projeto" />
+            </div>
+
             <div className="informacoes-projeto">
+              <div className="titulo-projeto">
+                <h2>Wine Experience</h2>
+              </div>
+              <div className="descricao">
                 <p>Projeto feito usando HTML, CSS e JavaScript</p>
                 <p>🔗 Ver no GitHub Pages</p>
+              </div>
             </div>
-        </a>
-    </div>
-
-    <div className="projeto">
-        <a href="#" target="_blank">
-            <img src="./src/assets/img/515x700.png" alt="Capa do projeto" />
-            <h3>Projeto 5</h3>
-            <div className="informacoes-projeto">
-                <p>Projeto feito usando HTML, CSS e JavaScript</p>
-                <p>🔗 Ver no GitHub Pages</p>
-            </div>
-        </a>
-    </div>
-
-    <div className="projeto">
-        <a href="#" target="_blank">
-            <img src="./src/assets/img/515x700.png" alt="Capa do projeto" />
-            <h3>Projeto 6</h3>
-            <div className="informacoes-projeto">
-                <p>Projeto feito usando HTML, CSS e JavaScript</p>
-                <p>🔗 Ver no GitHub Pages</p>
-            </div>
-        </a>
-    </div>
-
-    <div className="projeto">
-        <a href="#" target="_blank">
-            <img src="./src/assets/img/515x700.png" alt="Capa do projeto" />
-            <h3>Projeto 7</h3>
-            <div className="informacoes-projeto">
-                <p>Projeto feito usando HTML, CSS e JavaScript</p>
-                <p>🔗 Ver no GitHub Pages</p>
-            </div>
-        </a>
-    </div>
-
-    <div className="projeto">
-        <a href="#" target="_blank">
-            <img src="./src/assets/img/515x700.png" alt="Capa do projeto" />
-            <h3>Projeto 8</h3>
-            <div className="informacoes-projeto">
-                <p>Projeto feito usando HTML, CSS e JavaScript</p>
-                <p>🔗 Ver no GitHub Pages</p>
-            </div>
-        </a>
-    </div>
-</div>
-
-<button className="btn-mostrar-projetos">Mostrar Mais</button>
+          </a>
+        </div>
+      </div>
     </>
   );
 }
-
